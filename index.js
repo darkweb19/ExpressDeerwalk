045c00cd5e57ad98ea64657225b4b7d65eb8ea05
darkweb19 <sujanstha3220@gmail.com>
2023-03-15 14:05:06 +0545

    added categories

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ app.get("/user", (req, res) => {
 	res.send(users);
 });
 
+let categories = ["name", "age"];
+
 app.post("/add-user", (req, res) => {
 	if (req.body.name) {
 		users = [...users, ...req.body.name];
-
 		res.send("user added");
 	} else {
 		res.send("Please send the name");
 	}
+	console.log(req.body.name);
 });
 
 app.get("/delete-user", (req, res) => {
@@ -26,6 +28,27 @@ app.get("/delete-user", (req, res) => {
 		});
 		res.send("User Deleted");
 	}
+});
+
+app.get("/categories", (req, res) => {
+	res.send(categories);
+	console.log(categories);
+});
+
+app.post("/add-category", (req, res) => {
+	if (req.body.category) {
+		categories = [...categories, ...req.body.category];
+		res.send("Category added");
+	} else {
+		res.send("Please provide any category");
+	}
+});
+
+app.get("/delete-category", (req, res) => {
+	categories = categories.filter((cat) => {
+		return cat != req.query.name;
+	});
+	res.send("Category Deleted");
 });
 
 app.listen(3000, () => {
